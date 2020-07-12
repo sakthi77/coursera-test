6aee3ed947a8613eb02bfcfcfec56298f8943619
sakthi77 <67312875+sakthi77@users.noreply.github.com>
2020-07-12 11:32:56 +0530

    Add files via upload

---
+++ Nextpage.java
@@ -0,0 +1,15 @@
+package loginproject.validation;
+
+import javax.swing.*;
+import java.awt.*;
+  
+class Nextpage extends JFrame
+ {
+   Nextpage()
+  {
+  setDefaultCloseOperation(javax.swing.
+   WindowConstants.DISPOSE_ON_CLOSE);
+  setTitle("Welcome");
+  setSize(400, 200);
+   }
+  }
